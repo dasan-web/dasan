b5805fbdd8cfc9596f3a124fae8f27b616ccc29d
user <test@example.com>
2026-06-29 12:14:51 +0900

    update

--- src/components/CoreTechnology.tsx
+++ src/components/CoreTechnology.tsx
@@ -42,7 +42,7 @@ export default function CoreTechnology() {
                 >
                   <div className="w-full h-full relative">
                     <Image
-                      src="/clear_white_pills_mixed_sharp.png"
+                      src="/clear_white_pills_mixed_shapes_sharp.png"
                       alt="Dasan Core Technology"
                       fill
                       sizes="(max-w-768px) 180px, (max-w-1200px) 280px, 350px"
